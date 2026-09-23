e33329d173a88e36ea924a0472a2d4f6672bd6ef
Charles Goodsir <charlesguni2024@gmail.com>
2026-09-24 10:25:54 +1200

    Pipeline fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,6 +61,11 @@ resource "azurerm_key_vault" "main" {
   tenant_id                     = data.azurerm_client_config.current.tenant_id
   sku_name                      = "standard"
   purge_protection_enabled      = true
+  soft_delete_retention_days    = 7
   enable_rbac_authorization     = true
   public_network_access_enabled = false
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
 }

cb06a832d6db1cb739f62696efe343109b7f1f01
Charles Goodsir <charlesguni2024@gmail.com>
2026-09-25 17:17:29 +1200

    Fixing mistake

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,6 @@ resource "azurerm_subnet_network_security_group_association" "main" {
 #trivy:ignore:AZU-0058
 #trivy:ignore:AZU-0057
 resource "azurerm_storage_account" "main" {
-resource "azurerm_storage_account" "main" {
   name                              = "salzstcg314215"
   resource_group_name               = azurerm_resource_group.main.name
   location                          = azurerm_resource_group.main.location
